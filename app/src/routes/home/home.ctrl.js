c921ef2de7eb33bab6472ef1cf951ef0fb7182dc
HyangRim <curomame@naver.com>
2022-01-13 23:11:51 +0900

    sign up page 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,5 @@
 "use strict";
+const User = require("../../models/User");
 const UserStorage = require("../../models/UserStorage")
 
 const output = {
@@ -8,35 +9,22 @@ const output = {
   },
     login : (req,res) => {
     res.render("home/login");
+  },
+  register : (req, res) => {
+    res.render("home/register");
   }
-
 
 }
 
 
 
-
 const process = {
   login : (req, res) => {
-    const id = req.body.id;
-    const pwd = req.body.pwd;
 
-    const users = UserStorage.getUsers("id", "pwd");
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
 
-    const response = {};
-
-    if (users.id.includes(id)){
-      const idx = users.id.indexOf(id);
-      if (users.pwd[idx] === pwd){
-        response.success = true;
-        return res.json();
-      };
-    };
-    
-    response.success = false;
-    response.msg = '로그인에 실패';
-
-    return res.json(response)
   }
 };
 
